492af265569b72cd354f88e88a2646d7ca2381fd
ClairesCoding <61572429+ClairesCoding@users.noreply.github.com>
2020-03-13 13:08:48 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,19 @@
+ <footer class="container-fluid">
+          <div class="container">
+          <div class="row">
+            <div class="col-md-6 text-center fcontetn">
+              <p><?php bloginfo('name'); ?></p>
+               <img class="image" alt="twitter" src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/twitter.jpg">
+                <img class="image" alt="wifi" src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/wifi.jpg">
+              </div>
+            <div class="col-md-6 text-center fcontent">
+              <p>Terms and Conditions</p>
+                <img class="image" alt="linkedin" src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/linkedin.jpg">
+                <img class="image" alt="fb" src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/fb.jpg">
+              </div>
+            </div>
+          </div>
+      </footer>
+  </body>
+</html>
+<?php wp_footer(); ?>
