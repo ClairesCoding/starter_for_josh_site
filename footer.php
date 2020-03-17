9c1c3877b475989cec0097720e6f14d44dc10ce2
ClairesCoding <61572429+ClairesCoding@users.noreply.github.com>
2020-03-17 15:42:54 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
  <footer class="container-fluid">
           <div class="container">
           <div class="row">
-            <div class="col-md-6 text-center fcontetn">
+            <div class="col-md-6 text-center fcontent">
               <p><?php bloginfo('name'); ?></p>
                <img class="image" alt="twitter" src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/twitter.jpg">
                 <img class="image" alt="wifi" src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/wifi.jpg">
