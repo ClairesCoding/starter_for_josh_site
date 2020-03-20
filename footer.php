9bf0d436e6c3270d1c2fe9429b30167e2167c9ff
ClairesCoding <61572429+ClairesCoding@users.noreply.github.com>
2020-03-20 10:57:07 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,13 +3,13 @@
           <div class="row">
             <div class="col-md-6 text-center fcontent">
               <p><?php bloginfo('name'); ?></p>
-               <img class="image" alt="twitter" src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/twitter.jpg">
-                <img class="image" alt="wifi" src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/wifi.jpg">
+               <img class="image" alt="twitter" src="http://206.189.45.97/~mesh10/wp-content/themes/joshsite/images/twitter.jpg">
+                <img class="image" alt="wifi" src="http://206.189.45.97/~mesh10/wp-content/themes/joshsite/images/wifi.jpg">
               </div>
             <div class="col-md-6 text-center fcontent">
               <p>Terms and Conditions</p>
-                <img class="image" alt="linkedin" src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/linkedin.jpg">
-                <img class="image" alt="fb" src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/fb.jpg">
+                <img class="image" alt="linkedin" src="http://206.189.45.97/~mesh10/wp-content/themes/joshsite/images/linkedin.jpg">
+                <img class="image" alt="fb" src="http://206.189.45.97/~mesh10/wp-content/themes/joshsite/images/fb.jpg">
               </div>
             </div>
           </div>
